a3789b2f61486f53467ac77483750a9d0a77c8d5
sqautboy <migonyoung01@gmail.com>
2025-12-03 20:39:42 +0900

    feat: add SSM managed instance role for ECS Anywhere #8

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -21,3 +21,17 @@ output "task_role_name" {
   description = "Name of the ECS task IAM role"
   value       = aws_iam_role.ecs_task.name
 }
+
+#------------------------------------------------------------------------------
+# SSM Managed Instance Role Outputs
+#------------------------------------------------------------------------------
+
+output "ssm_role_arn" {
+  description = "ARN of the SSM managed instance IAM role"
+  value       = aws_iam_role.ssm_managed_instance.arn
+}
+
+output "ssm_role_name" {
+  description = "Name of the SSM managed instance IAM role"
+  value       = aws_iam_role.ssm_managed_instance.name
+}
